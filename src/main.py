629ce0de28a3a317ca20828f80e1f4094dafdb74
root <root@localhost>
2021-10-02 23:55:22 +0530

    auto publish 1633199120288

---
+++ src/main.py
@@ -0,0 +1,12 @@
+print("what is you age")
+
+age=int(input())
+
+if age<18:
+	print("you cannot drive")
+elif age==18:
+	print("we will decide")
+elif age>100:
+	print("you are not human")
+else:
+	print("you can drive")
